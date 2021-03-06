afac22780db4ccaef2a89117ad221f05d46de3de
Victor Milovanov <lost@losttech.software>
2021-03-06 11:52:55 -0800

    better test coverage for Array

--- test/ArrayTests.cs
+++ test/ArrayTests.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 using LostTech.LargeCollections;
 
@@ -22,6 +21,51 @@ namespace Memory
             using var arr = new Array<byte>(LargeSize);
             arr[LargeSize - 1] = 42;
             Assert.Equal(42, arr[LargeSize - 1]);
+        }
+
+        [Fact]
+        public void ZeroSized()
+        {
+            using var arr = new Array<int>(0);
+            Assert.Equal(arr.Length, (nuint)0);
+        }
+
+        [Fact]
+        public void BoundsCheck_Get()
+        {
+            using var arr = new Array<int>(42);
+            Assert.Throws<IndexOutOfRangeException>(() => arr[42]);
+        }
+
+        [Fact]
+        public void BoundsCheck_Set()
+        {
+            using var arr = new Array<int>(42);
+            Assert.Throws<IndexOutOfRangeException>(() => arr[42] = 1);
+        }
+
+        [Fact]
+        public void DisposedCheck_Get()
+        {
+            var arr = new Array<int>(42);
+            arr.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => arr[1]);
+        }
+
+        [Fact]
+        public void DisposedCheck_Set()
+        {
+            var arr = new Array<int>(42);
+            arr.Dispose();
+            Assert.Throws<ObjectDisposedException>(() => arr[1] = 42);
+        }
+
+        [Fact]
+        public void DoubleDispose()
+        {
+            var arr = new Array<int>(42);
+            arr.Dispose();
+            arr.Dispose();
         }
 
         nuint LargeSize
